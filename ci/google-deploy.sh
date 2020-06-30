2d946eadfe224fbcc16baa012a5c42eefef17536
Craig Lewis <craigericlewis@gmail.com>
2020-06-30 12:49:45 -0400

    Swap to copy-files

--- ci/google-deploy.sh
+++ ci/google-deploy.sh
@@ -8,7 +8,7 @@
 cd ../
 
 # copy the application files
-gcloud compute scp [!.]* ${CLOUD_USER}@${INSTANCE_NAME}:./ --quiet --zone ${INSTANCE_ZONE}
+gcloud compute copy-files [!.]* ${CLOUD_USER}@${INSTANCE_NAME}:./ --quiet --zone ${INSTANCE_ZONE}
 
 # deploy the application
 gcloud compute ssh ${CLOUD_USER}@${INSTANCE_NAME} --zone ${INSTANCE_ZONE} --command "sudo docker-compose -f ./ci/docker-compose.prod.yaml up --build"
