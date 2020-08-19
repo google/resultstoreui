3c41d8b339d473cce90eb47f97ea81462dc32475
Craig Lewis <craigericlewis@gmail.com>
2020-08-19 15:20:51 -0400

    Reorganize project. Package python server. Update Tests

---
+++ resultstoresearch/client/src/components/SearchWrapper/SearchBar/index.tsx
@@ -0,0 +1,168 @@
+import React from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import * as invocation_pb from '../../../api/invocation_pb';
+import SearchTooltip from './SearchTooltip';
+import SearchButton from './SearchButton';
+import FlakyTestButton from './FlakyTestButton';
+import { State as PageWrapperState } from '../';
+
+interface QueryPrefix {
+    value: string;
+    label: string;
+}
+
+const searchPlaceholder = 'Search Invocations';
+const queryPrefixes: Array<QueryPrefix> = [
+    {
+        value: 'invocation_attributes.users:',
+        label: 'invocation_attributes.users',
+    },
+    {
+        value: 'invocation_attributes.labels:',
+        label: 'invocation_attributes.labels',
+    },
+    { value: 'name=', label: 'name' },
+    { value: 'status_attributes.status=', label: 'status_attributes.status' },
+    { value: 'workspace_info.hostname=', label: 'workspace_info.hostname' },
+    {
+        value: 'timing.start_time',
+        label: 'timing.start_time',
+    },
+];
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        margin: {
+            margin: theme.spacing(1),
+        },
+        root: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexGrow: 2,
+        },
+    })
+);
+
+export interface State {
+    query: string;
+    invocations: Array<invocation_pb.Invocation>;
+}
+
+export interface SearchBarProps {
+    setQueryParent: (query: PageWrapperState['query']) => void;
+    queryParent: PageWrapperState['query'];
+    hasError: boolean;
+    loading: boolean;
+    onFlakyTestClick?: () => void;
+    showSearchButton?: boolean;
+    showFlakyTestButton?: boolean;
+    flakyDisabled: boolean;
+    searchDisabled: boolean;
+}
+
+/*
+SearchBar component to search for invocations by query string
+*/
+const SearchBar: React.FC<SearchBarProps> = ({
+    setQueryParent,
+    queryParent,
+    hasError,
+    loading,
+    showSearchButton = true,
+    showFlakyTestButton = true,
+    onFlakyTestClick,
+    flakyDisabled,
+    searchDisabled,
+}) => {
+    const [query, setQuery] = React.useState<State['query']>('');
+    const classes = useStyles();
+
+    const onChange = (_, value: QueryPrefix, reason) => {
+        if (reason === 'select-option') {
+            const splitQuery = query.split(' ');
+            splitQuery[splitQuery.length - 1] = value.value;
+            const newQuery = splitQuery.join(' ');
+            setQuery(newQuery);
+            return newQuery;
+        }
+    };
+
+    const updateQuery = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            setQueryParent(query);
+        }
+    };
+
+    const filterOptions = (options: Array<QueryPrefix>) => {
+        const splitQuery = query.split(' ');
+        const latestQuery = splitQuery[splitQuery.length - 1];
+        const filteredOptions = options.filter((option) => {
+            return option.label.includes(latestQuery);
+        });
+        return filteredOptions;
+    };
+
+    return (
+        <>
+            <div className={classes.root}>
+                <FormControl
+                    fullWidth
+                    className={classes.margin}
+                    variant="outlined"
+                    error={hasError}
+                >
+                    <Autocomplete
+                        id="search-bar"
+                        options={queryPrefixes}
+                        getOptionLabel={(option: QueryPrefix) => option.label}
+                        selectOnFocus={false}
+                        inputValue={query}
+                        value={''}
+                        clearOnBlur={false}
+                        onChange={onChange}
+                        onKeyDown={(e) => updateQuery(e)}
+                        onInputChange={(_, value: string) => setQuery(value)}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                variant="outlined"
+                                InputProps={{
+                                    ...params.InputProps,
+                                    startAdornment: (
+                                        <InputAdornment position="end">
+                                            <SearchTooltip />
+                                        </InputAdornment>
+                                    ),
+                                }}
+                                placeholder={searchPlaceholder}
+                            ></TextField>
+                        )}
+                        filterOptions={filterOptions}
+                    ></Autocomplete>
+                </FormControl>
+            </div>
+            {showSearchButton && (
+                <SearchButton
+                    showSpinner={loading}
+                    onClick={() => setQueryParent(query)}
+                    disabled={searchDisabled || flakyDisabled}
+                />
+            )}
+            {showFlakyTestButton && (
+                <FlakyTestButton
+                    showSpinner={flakyDisabled}
+                    onClick={onFlakyTestClick}
+                    disabled={
+                        flakyDisabled || searchDisabled || queryParent === ''
+                    }
+                />
+            )}
+        </>
+    );
+};
+
+export default SearchBar;
---
+++ resultstoresearch/e2e/cypress/integration/app.spec.ts
@@ -0,0 +1,65 @@
+context("ResultStoreSearch Home Page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Should display an error if the query is invalid", () => {
+    cy.get('input[id="search-bar"]').type("incorrect query");
+    cy.get('input[id="search-bar"]').type("{enter}");
+    cy.get('p[id="search-error"]').contains("Invalid query string");
+  });
+
+  it("Should render the invocations in the table", () => {
+    cy.get('input[id="search-bar"]').type("a");
+    cy.get('input[id="search-bar"]').type("{enter}");
+    cy.get('div[aria-label="row"]').contains(
+      "invocations/51be7217-9798-4448-adf8-1e4428c71e9e"
+    );
+    cy.get('div[aria-label="row"]').contains("TESTING");
+    cy.get('div[aria-label="row"]').contains("dank,meme");
+    cy.get('div[aria-label="row"]').contains("00:06");
+    cy.get('div[aria-label="row"]').contains("lewiscraig@");
+  });
+
+  it("Should display tool1 and tool2 in dropdown and have 1 invocation", () => {
+    cy.get('input[id="search-bar"]').type("a");
+    cy.get('input[id="search-bar"]').type("{enter}");
+    cy.get('div[id="tool-select"]').click();
+    cy.get('li[data-value="tool1"]').contains("tool1");
+    cy.get('li[data-value="tool2"').contains("tool2");
+    cy.get('li[data-value="tool2"]').click();
+    cy.get('input[id="search-bar"]').type("i");
+    cy.get('input[id="search-bar"]').type("{enter}");
+    cy.get('input[id="search-bar"]').type("i");
+    cy.get('input[id="search-bar"]').type("{enter}");
+    cy.get('div[id="InvocationTable"]')
+      .find('div[aria-label="row"]')
+      .should("have.length", 1);
+  });
+
+  it("Should display tooltip on hover", () => {
+    cy.get('svg[aria-label="SearchHelp"]').trigger("mouseover");
+    cy.get('div[class="MuiTooltip-popper"]').contains(
+      'Fields that support equals ("=") restrictions:'
+    );
+  });
+
+  it("Should open the file modal on file button click", () => {
+    cy.get('input[id="search-bar"]').type("a");
+    cy.get('input[id="search-bar"]').type("{enter}");
+    cy.get('button[title="Close"]').click();
+    cy.get('div[aria-rowindex="1"]').trigger("mouseover");
+    cy.get('button[id="FileButton-0"]').click();
+    cy.get('div[id="FileModal"]').should("have.length", 1);
+  });
+
+  it("Should render files in the file table", () => {
+    cy.get('input[id="search-bar"]').type("a");
+    cy.get('input[id="search-bar"]').type("{enter}");
+    cy.get('button[title="Close"]').click();
+    cy.get('div[aria-rowindex="1"]').trigger("mouseover");
+    cy.get('button[id="FileButton-0"]').click();
+    cy.get('div[id="InvocationModalRow"]').click();
+    cy.get('div[id="FileTable"]').contains("test-file");
+  });
+});
